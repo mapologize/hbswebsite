0eee593effec14a505997973cc96141a68681c80
Penzer <dedonoiizunfinity@gmail.com>
2023-12-07 00:18:51 +0700

    now commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const hbs = require('hbs');
+const generalRouter = require('./routes/general');
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'hbs');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
+
+hbs.registerPartials(__dirname + '/views/partials');    
+
+app.use('/', generalRouter);
+
+app.listen(port,() => {
+    console.log('app listen port :' + port);
+});
